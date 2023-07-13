8009a519096c4c2579bc651ee2c963e0b63a8772
garrisonhh <garrisonhh@pm.me>
2023-07-13 14:16:27 -0400

    streaming input blocks

---
+++ src/blockstream.zig
@@ -0,0 +1,258 @@
+//! an iterator for streaming read-mmapped blocks of lines from a file
+
+const std = @import("std");
+const os = std.os;
+const linux = os.linux;
+const Allocator = std.mem.Allocator;
+const page_size = std.mem.page_size;
+
+const Self = @This();
+
+const RawBlock = []align(page_size) const u8;
+
+path: []const u8,
+handle: os.fd_t,
+length: usize,
+
+const OpenError = os.OpenError || os.SeekError;
+
+/// open file for streaming
+fn open(path: []const u8) OpenError!Self {
+    const fd = try os.open(path, linux.O.RDONLY, 0);
+
+    // detect length
+    try os.lseek_END(fd, 0);
+    const length = try os.lseek_CUR_get(fd);
+    try os.lseek_SET(fd, 0);
+
+    return Self{
+        .path = path,
+        .handle = fd,
+        .length = length,
+    };
+}
+
+/// close stream
+fn close(self: Self) void {
+    os.close(self.handle);
+}
+
+/// small wrapper for easy mmap usage consistency
+fn map(self: Self, offset: usize, length: usize) os.MMapError!RawBlock {
+    return std.os.mmap(
+        null,
+        length,
+        linux.PROT.READ,
+        linux.MAP.SHARED,
+        self.handle,
+        offset,
+    );
+}
+
+/// higher level block abstraction, better for consumption
+pub const Block = struct {
+    mem: []align(page_size) const u8,
+    text: []const u8,
+
+    fn map(bs: Self, range: BlockRange) os.MMapError!Block {
+        const mem = try bs.map(range.aligned_offset, range.aligned_length);
+        const text = mem[range.offset - range.aligned_offset..range.length];
+
+        return .{
+            .mem = mem,
+            .text = text,
+        };
+    }
+
+    fn unmap(blk: Block) void {
+        // TODO pretty sure this is thread-safe but not entirely
+        os.munmap(blk.mem);
+    }
+};
+
+const BlockRange = struct {
+    /// actual offset
+    offset: usize,
+    /// actual length
+    length: usize,
+    /// offset for mmap
+    aligned_offset: usize,
+    /// length for mmap
+    aligned_length: usize,
+
+    fn init(offset: usize, length: usize) BlockRange {
+        const offset_diff = offset % page_size;
+        const aligned_offset = offset - offset_diff;
+
+        const ext_length = length + offset_diff;
+        const length_diff = ext_length % page_size;
+        const aligned_length =
+            ext_length - length_diff +
+            @as(usize, if (length_diff > 0) page_size else 0);
+
+        std.debug.assert(aligned_offset <= offset);
+        std.debug.assert(aligned_offset + aligned_length >= offset + length);
+        std.debug.assert(aligned_offset % page_size == 0);
+        std.debug.assert(aligned_length % page_size == 0);
+
+        return .{
+            .offset = offset,
+            .length = length,
+            .aligned_offset = aligned_offset,
+            .aligned_length = aligned_length,
+        };
+    }
+};
+
+const ScanError =
+    Allocator.Error ||
+    os.MMapError ||
+    error{BadSizeHintAlignment};
+
+/// find blocks appropriate for streaming. it attempts to allocate blocks at a
+/// maximum size below size_hint, but if there is a line that is larger than
+/// size_hint, will still create a range, at the smallest size possible.
+///
+/// size_hint must be a multiple of page_size for mmap alignment
+/// returns owned slice from allocator
+fn scan(
+    self: Self,
+    ally: Allocator,
+    size_hint: usize,
+) ScanError![]const BlockRange {
+    if (size_hint % page_size != 0) {
+        return ScanError.BadSizeHintAlignment;
+    }
+
+    var ranges = std.ArrayListUnmanaged(BlockRange){};
+
+    var range_start: usize = 0;
+    var range_len: usize = 0;
+    var offset: usize = 0;
+    while (offset < self.length) : (offset += size_hint) {
+        const is_eof = offset + size_hint >= self.length;
+
+        // map scannable block
+        const mem_length = if (is_eof) self.length - offset else size_hint;
+        const mem = try self.map(offset, mem_length);
+        defer os.munmap(mem);
+
+        // scan for next range
+        for (mem, 0..) |ch, index| {
+            if (ch == '\n') {
+                const length = offset + index - range_start;
+                if (length < size_hint) {
+                    // range could still be larger
+                    range_len = length;
+                    continue;
+                }
+
+                // detected a range
+                if (range_len == 0) {
+                    range_len = length;
+                }
+
+                const range = BlockRange.init(range_start, range_len);
+                try ranges.append(ally, range);
+
+                range_start += range_len;
+                range_len = 0;
+            }
+        }
+
+        // for the final range, just generate a range (if it exists)
+        if (is_eof and range_start < self.length) {
+            const range = BlockRange.init(
+                range_start,
+                self.length - range_start,
+            );
+
+            try ranges.append(ally, range);
+        }
+    }
+
+    return try ranges.toOwnedSlice(ally);
+}
+
+// iterator ====================================================================
+
+/// returns a thread-safe iterator over multiple files
+/// paths should outlive iterator
+pub fn iterator(
+    /// list of files to read
+    paths: []const []const u8,
+    /// block size hint
+    size_hint: usize,
+) Iterator {
+    return Iterator.init(paths, size_hint);
+}
+
+pub const Iterator = struct {
+    pub const Error = OpenError || ScanError;
+
+    paths_remaining: []const []const u8,
+    size_hint: usize,
+
+    /// state
+    gpa: std.heap.GeneralPurposeAllocator(.{}) = .{},
+    gpa_killed: bool = false,
+    mutex: std.Thread.Mutex = .{},
+
+    bs: ?Self = null,
+    ranges: []const BlockRange = &.{},
+    index: usize = 0,
+
+    fn init(paths: []const []const u8, size_hint: usize) Iterator {
+        return .{
+            .paths_remaining = paths,
+            .size_hint = size_hint,
+        };
+    }
+
+    /// assumes mutex lock
+    fn nextRange(iter: *Iterator) Error!?BlockRange {
+        // there are ranges remaining, nothing to do but iterate
+        if (iter.index < iter.ranges.len) {
+            defer iter.index += 1;
+            return iter.ranges[iter.index];
+        }
+
+        // close/free any resources this is done with
+        if (iter.bs) |bs| {
+            bs.close();
+            iter.bs = null;
+
+            const ally = iter.gpa.allocator();
+            ally.free(iter.ranges);
+        }
+
+        // check if streams remain
+        if (iter.paths_remaining.len == 0) {
+            if (!iter.gpa_killed) {
+                _ = iter.gpa.deinit();        
+                iter.gpa_killed = true;
+            }
+            return null;
+        }
+
+        // open new stream
+        const ally = iter.gpa.allocator();
+        const bs = try open(iter.paths_remaining[0]);
+
+        iter.bs = bs;
+        iter.paths_remaining = iter.paths_remaining[1..];
+        iter.ranges = try bs.scan(ally, iter.size_hint);
+        iter.index = 1;
+
+        return iter.ranges[iter.index];
+    }
+
+    /// remember to unmap block when you're done with it
+    pub fn next(iter: *Iterator) Error!?Block {
+        iter.mutex.lock();
+        defer iter.mutex.unlock();
+
+        const range = (try iter.nextRange()) orelse return null;
+        return try Block.map(iter.bs.?, range);
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const stdout = std.io.getStdOut().writer();
 const stderr = std.io.getStdErr().writer();
 const Allocator = std.mem.Allocator;
 const Progress = std.Progress;
+const BlockStream = @import("blockstream.zig");
 
 // gpa =========================================================================
 
@@ -31,7 +32,7 @@ const Hash = packed struct(u32) {
 
 const Password = struct {
     const Self = @This();
-    
+
     const LengthInt = u32;
 
     hash: Hash,
@@ -49,7 +50,7 @@ const PasswordSet = struct {
 
     const Chain = struct {
         mutex: std.Thread.Mutex = .{},
-        list: std.ArrayListUnmanaged(Password) = .{},
+        list: std.MultiArrayList(Password) = .{},
     };
 
     ally: Allocator,
@@ -82,10 +83,13 @@ const PasswordSet = struct {
         chain.mutex.lock();
         defer chain.mutex.unlock();
 
-        for (chain.list.items) |pw| {
-            if (pw.hash.eql(hash) and std.mem.eql(u8, pw.str(), str)) {
-                // found match!
-                break;
+        for (chain.list.items(.hash), 0..) |other_hash, i| {
+            if (other_hash.eql(hash)) {
+                const other_str = chain.list.get(i).str();
+                if (std.mem.eql(u8, str, other_str)) {
+                    // found match!
+                    break;
+                }
             }
         } else {
             // no match, add a link
@@ -116,8 +120,9 @@ const PasswordSet = struct {
 
         // iterate over counter and write
         for (self.chains) |chain| {
-            for (chain.list.items) |pw| {
-                try strm.print("{s}\n", .{ pw.str() });
+            for (chain.list.items(.ptr), chain.list.items(.len)) |ptr, len| {
+                const str = ptr[0..len];
+                try strm.print("{s}\n", .{str});
                 prog_node.completeOne();
             }
         }
@@ -128,253 +133,33 @@ const PasswordSet = struct {
 
 // file management =============================================================
 
-const MappedFile = struct {
-    const Self = @This();
-
-    const Block = struct {
-        /// required for munmap, not usable (contains garbage at the end)
-        memory: []align(std.mem.page_size) u8,
-        /// useful text
-        text: []const u8,
-    };
-
-    const InitError =
-        std.os.OpenError ||
-        std.os.MMapError ||
-        std.os.SeekError ||
-        error { LongLineInInput };
-
-    /// if you get an OOM error, it is likely that this is too large.
-    ///
-    /// however, this *must* be more than one page_size for iteration to work
-    /// when creating blocks
-    const max_block_bytes = 4096 * std.mem.page_size;
-
-    fd: std.os.fd_t,
-    blocks: std.ArrayListUnmanaged(Block),
-
-    /// open and mmap a file from a path
-    /// TODO split this up for readability if I need to return to it
-    fn init(ally: Allocator, path: []const u8) InitError!Self {
-        // open file for reading
-        const fd = try std.os.open(path, std.os.linux.O.RDONLY, 0);
-
-        // get length
-        try std.os.lseek_END(fd, 0);
-        const byte_length = try std.os.lseek_CUR_get(fd);
-        try std.os.lseek_SET(fd, 0);
-
-        // mmap blocks
-        const PROT = std.os.linux.PROT;
-        const MAP = std.os.linux.MAP;
-        
-        var blocks = std.ArrayListUnmanaged(Block){};
-
-        var offset: usize = 0; // offset of current block
-        var text_offset: usize = 0; // offset of text into the next block
-        while (offset < byte_length) {
-            // acq memory
-            const max_bytes = @min(byte_length - offset, max_block_bytes);
-            const memory = try std.os.mmap(
-                null,
-                max_bytes,
-                PROT.READ,
-                MAP.SHARED,
-                fd,
-                offset,
-            );
-
-            // trim to line boundary if there's another block after this one
-            var text: []const u8 = memory[text_offset..];
-            if (offset + memory.len < byte_length) {
-                // get memory window for text (ends at final newline) 
-                var boundary = memory.len - 1;
-
-                while (boundary > text_offset and memory[boundary] != '\n') {
-                    boundary -= 1;
-                }
-
-                text = memory[text_offset..boundary];
-
-                // find next offset and text offset
-                const aligned_offset =
-                    @divFloor(boundary, std.mem.page_size) *
-                    std.mem.page_size;
-
-                if (aligned_offset == 0) {
-                    return InitError.LongLineInInput;
-                }
-
-                offset += aligned_offset;
-                text_offset = boundary - aligned_offset;
-            } else {
-                // we're done
-                offset = byte_length;
-            }
-
-            try blocks.append(ally, Block{
-                .memory = memory,
-                .text = text,
-            });
-        }
-
-        return Self{
-            .fd = fd,
-            .blocks = blocks,
-        };
-    }
-
-    /// munmap and close the file
-    fn deinit(self: *Self, ally: Allocator) void {
-        for (self.blocks.items) |block| std.os.munmap(block.memory);
-        self.blocks.deinit(ally);
-        std.os.close(self.fd);
-    }
-
-    /// iterate over passwords
-    fn iterator(self: Self) Iterator {
-        return .{ .blocks = self.blocks.items };
-    }
-
-    const Iterator = struct {
-        /// blocks remaining
-        blocks: []const Block,
-        /// current tokenizer
-        tokenizer: ?std.mem.TokenIterator(u8, .scalar) = null,
-
-        fn next(iter: *Iterator) ?[]const u8 {
-            // null case; get next block
-            if (iter.tokenizer == null) {
-                if (iter.blocks.len == 0) {
-                    return null;
-                }
-
-                const block = iter.blocks[0];
-                iter.blocks = iter.blocks[1..];
-
-                iter.tokenizer = std.mem.tokenizeScalar(u8, block.text, '\n');
-            }
-
-            // get a token
-            if (iter.tokenizer.?.next()) |token| {
-                return token;
-            }
-
-            // no tokens remaining, recurse to try the next block
-            iter.tokenizer = null;
-            return iter.next();
-        }
-    };
-};
-
-/// it fucks those files lmao
-const FileFucker = struct {
-    const Self = @This();
-
-    files: std.ArrayListUnmanaged(MappedFile) = .{},
-
-    fn deinit(self: *Self, ally: Allocator) void {
-        for (self.files.items) |*file| file.deinit(ally);
-        self.files.deinit(ally);
-    }
-
-    /// queue a file to be deduped
-    fn add(self: *Self, ally: Allocator, path: []const u8) !void {
-        const file = try MappedFile.init(ally, path);
-        try self.files.append(ally, file);
-    }
-
-    fn dedupBlock(set: *PasswordSet, block: MappedFile.Block) !void {
+fn dedupEntry(set: *PasswordSet, iter: *BlockStream.Iterator) !void {
+    while (try iter.next()) |block| {
         var passwords = std.mem.tokenizeScalar(u8, block.text, '\n');
-        while (passwords.next()) |str| {
-            if (str.len == 0) continue;
-            try set.add(str);
+        while (passwords.next()) |password| {
+            if (password.len == 0) continue;
+            try set.add(password);
         }
     }
+}
 
-    fn dedupEntry(set: *PasswordSet, b: *Blockerator) !void {
-        while (b.next()) |block| {
-            try dedupBlock(set, block);
-        }
-        
-        try std.Thread.yield();
+/// threaded dedup with all the files
+fn dedup(set: *PasswordSet, paths: []const []const u8) !void {
+    const size_hint = 512 * std.mem.page_size; // TODO make this configurable?
+    var iter = BlockStream.iterator(paths, size_hint);
+
+    const cpu_count = try std.Thread.getCpuCount();
+    var threads = std.BoundedArray(std.Thread, 256){};
+
+    for (0..cpu_count) |_| {
+        const thread = try std.Thread.spawn(.{}, dedupEntry, .{ set, &iter });
+        try threads.append(thread);
     }
 
-    /// threaded dedup with all the files
-    fn dedup(self: *const Self, set: *PasswordSet) !void {
-        var progress = std.Progress{};
-        var b = self.blockerator(&progress);
-
-        const cpu_count = try std.Thread.getCpuCount();    
-        var threads = std.BoundedArray(std.Thread, 256){};
-
-        for (0..cpu_count) |_| {
-            const thread = try std.Thread.spawn(.{}, dedupEntry, .{set, &b});
-            try threads.append(thread);
-        }
-
-        for (threads.slice()) |thread| {
-            thread.join();
-        }
+    for (threads.slice()) |thread| {
+        thread.join();
     }
-
-    fn blockerator(self: *const Self, progress: *Progress) Blockerator {
-        var b = Blockerator{
-            .files = self.files.items,
-            .prog_node = undefined,
-        };
-
-        // count blocks
-        var total_blocks: usize = 0;
-        for (b.files) |file| {
-            total_blocks += file.blocks.items.len;
-        }
-
-        // start progress
-        b.prog_node = progress.start("processing blocks", total_blocks);
-
-        return b;
-    }
-
-    /// thread-safe block queue
-    const Blockerator = struct {
-        mutex: std.Thread.Mutex = .{},
-        
-        prog_node: *Progress.Node,
-        
-        files: []const MappedFile,
-        file: usize = 0,
-        block: usize = 0,
-        done: bool = false,
-
-        fn next(b: *Blockerator) ?MappedFile.Block {
-            b.mutex.lock();
-            defer b.mutex.unlock();
-
-            if (b.done) return null;
-            
-            // get block
-            const blocks = b.files[b.file].blocks.items;
-            const block = blocks[b.block];
-
-            b.prog_node.completeOne();
-
-            // iterate
-            b.block += 1;
-            if (b.block >= blocks.len) {
-                b.file += 1;
-                b.block = 0;
-
-                if (b.file >= b.files.len) {
-                    b.done = true;
-                    b.prog_node.end();
-                }
-            }
-
-            return block;
-        }
-    };
-};
+}
 
 // progress ====================================================================
 
@@ -388,9 +173,6 @@ pub fn main() !void {
 
     var set = try PasswordSet.init(arena_ally);
     defer set.deinit();
-
-    var fucker = FileFucker{};
-    defer fucker.deinit(ally);
 
     // get input file path from args
     const args = try std.process.argsAlloc(ally);
@@ -415,13 +197,8 @@ pub fn main() !void {
     const out_filepath = args[1];
     const in_filepaths = args[2..];
 
-    // load files
-    for (in_filepaths) |in_filepath| {
-        try fucker.add(ally, in_filepath);
-    }
-
     // do the stuff
-    try fucker.dedup(&set);
+    try dedup(&set, in_filepaths);
 
     // write to the output file
     try set.extrude(out_filepath);
